6d8e7ad5b426894936d3a40a67982d6ff8f2fbf8
Erick Taco <ericktaco68@gmail.com>
2023-04-11 13:38:54 -0500

    Paginas del centro cultural v1.5

---
+++ web/batumba.php
@@ -0,0 +1,48 @@
+<?php
+include('../comunes/header.php');
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Batumba</title>
+	<style>
+		.descripcion {
+			font-size: 1.2em;
+			font-style: italic;
+		}
+
+		.container {
+			display: flex;
+			flex-wrap: wrap;
+			justify-content: center;
+			align-items: center;
+			padding: 20px;
+		}
+
+		.container img {
+			width: 250px;
+			height: 250px;
+			object-fit: cover;
+			margin: 10px;
+		}
+	</style>
+</head>
+
+<body>
+
+	<center>Descripción del Grupo Artistico</center>
+	</h2>
+	<p class="descripcion">Descripción de la página. Aquí puedes escribir todo lo que quieras sobre el contenido que se muestra en la galería de fotos.</p>
+	<div class="container">
+		<img src="../img/BATUMBA/foto 11.jpg" alt="Foto 1">
+		<img src="../img/BATUMBA/IMG_5706.jpg" alt="Foto 2">
+		<img src="../img/BATUMBA/IMG_5707.jpg" alt="Foto 3">
+		<img src="../img/BATUMBA/IMG_6282.jpg" alt="Foto 4">
+	</div>
+</body>
+<?php
+include('../comunes/footer.php');
+?>
+
+</html>
